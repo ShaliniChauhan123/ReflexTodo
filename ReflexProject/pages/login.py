9039cc4998f63d1aed33cf817c0855de19b83c62
Shalini <chauhanshaliniuk@gmail.com>
2024-04-04 15:53:16 +0530

    login basic ui

--- ReflexProject/pages/login.py
+++ ReflexProject/pages/login.py
@@ -1,4 +1,31 @@
 import reflex as rx
 
 def login():
-    return rx.heading("This is my component")
+    return  rx.form(
+        rx.vstack(
+            rx.input.root(
+                rx.input(
+                    name="email_id",
+                    placeholder="Enter your email id...",
+                    bg="white",
+                    style={"border": "1px solid #007bff"}
+                ),
+                width="100%",
+            ),
+               rx.input.root(
+                rx.input(
+                    name="password",
+                    placeholder="Enter your password...",
+                    bg="white",
+                    style={"border": "1px solid #007bff"}
+                ),
+                width="100%",
+            ),
+            
+
+            rx.button("Submit"),
+        ),
+        # on_submit=State.add_todo,
+        reset_on_submit=True,
+        width="100%",
+    )
