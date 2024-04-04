63e9083e8c94e3ab4a678666c91ccbce23ff6a14
Shalini <chauhanshaliniuk@gmail.com>
2024-04-04 16:21:43 +0530

    login page with ui

--- ReflexProject/pages/login.py
+++ ReflexProject/pages/login.py
@@ -1,31 +1,41 @@
 import reflex as rx
 
 def login():
-    return  rx.form(
+    return  rx.center(
+   rx.vstack(
+    rx.form(
         rx.vstack(
-            rx.input.root(
+           rx.flex(
+                rx.input.root(
                 rx.input(
                     name="email_id",
                     placeholder="Enter your email id...",
                     bg="white",
-                    style={"border": "1px solid #007bff"}
+                    style={"padding":"24px"}
                 ),
                 width="100%",
+                style={"margin-bottom":"24px"}
             ),
                rx.input.root(
                 rx.input(
                     name="password",
                     placeholder="Enter your password...",
                     bg="white",
-                    style={"border": "1px solid #007bff"}
+                   style={"padding":"24px"}
                 ),
                 width="100%",
+                style={"margin-bottom":"24px"}
             ),
             
 
-            rx.button("Submit"),
+            rx.button("Login", style={"padding":"24px"}),
+            style={"flex-direction":"column","width":"100%"}
+           )
         ),
         # on_submit=State.add_todo,
         reset_on_submit=True,
         width="100%",
+    ),
+      style={"margin": "80px", "width": "50%","padding":"36px", "border-radius":"12px","background-color":"rgba(237, 231, 225)"}
+)
     )
