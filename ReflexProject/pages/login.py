18b65908f0abcfe1f0a46784f04441ee567bfc7d
Shalini <chauhanshaliniuk@gmail.com>
2024-04-05 16:01:48 +0530

    added login auth token and redirection

--- ReflexProject/pages/login.py
+++ ReflexProject/pages/login.py
@@ -1,4 +1,56 @@
 import reflex as rx
+from sqlmodel import select
+
+from ..base_state import State
+from ..user import User
+class LoginState(State):
+    """Handle login form submission and redirect to proper routes after authentication."""
+
+    error_message: str = ""
+    redirect_to: str = ""
+
+    def on_submit(self, form_data) -> rx.event.EventSpec:
+        """Handle login form on_submit.
+
+        Args:
+            form_data: A dict of form fields and values.
+        """
+        self.error_message = ""
+        email_id = form_data["email_id"]
+        password = form_data["password"]
+        with rx.session() as session:
+            user = session.exec(
+                select(User).where(User.email_id == email_id)
+            ).one_or_none()
+        # if user is not None and not user.enabled:
+        #     self.error_message = "This account is disabled."
+        #     return rx.set_value("password", "")
+        if user is None or not user.verify(password):
+            self.error_message = "There was a problem logging in, please try again."
+            return rx.set_value("password", "")
+        if (
+            user is not None
+            and user.id is not None
+            and user.enabled
+            and user.verify(password)
+        ):
+            # mark the user as logged in
+            self._login(user.id)
+        self.error_message = ""
+        return LoginState.redir()  # type: ignore
+
+    def redir(self):
+        """Redirect to the redirect_to route if logged in, or to the login page if not."""
+        if not self.is_hydrated:
+            # wait until after hydration to ensure auth_token is known
+            return LoginState.redir()  # type: ignore
+        page = self.router.page.path
+        if not self.is_authenticated and page != "/login":
+            self.redirect_to = page
+            return rx.redirect("/login")
+        elif page == "/login":
+            return rx.redirect(self.redirect_to or "/")
+
 
 def login():
     return  rx.center(
@@ -32,7 +84,7 @@ def login():
             style={"flex-direction":"column","width":"100%"}
            )
         ),
-        # on_submit=State.add_todo,
+        on_submit=LoginState.on_submit,
         reset_on_submit=True,
         width="100%",
     ),
