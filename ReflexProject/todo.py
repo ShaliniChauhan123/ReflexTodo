1062f97a5b0889519bce4f74cd5cffe2a74d4b6a
Shalini <chauhanshaliniuk@gmail.com>
2024-04-09 12:47:41 +0530

    update todo schema

--- ReflexProject/todo.py
+++ ReflexProject/todo.py
@@ -12,7 +12,7 @@ class Todo(
     """A local Todo model with todo_id, todo, is_completed """
 
 
-    user_id:str=Field(unique=True,nullable=False,index=True)
+    user_id:str=Field(nullable=False,index=True)
     todo:str=Field(nullable=False)
     is_completed:bool=Field(nullable=False)
 
